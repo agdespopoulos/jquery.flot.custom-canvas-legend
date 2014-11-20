ef6957c90c6341a2e4ecb81bb4ba3a52470378b0
Carl Schroedl <cschroedl@usgs.gov>
2014-11-20 15:31:28 -0600

    Consolidating renderers and sizers.

---
+++ src/main/resources/jquery.flot.custom-canvas-legend.renderersAndSizers.js
@@ -0,0 +1,74 @@
+/* global $ Math */
+(function () {
+    $.plot.custom_canvas_legend = $.plot.custom_canvas_legend || {};
+
+    var boxLeftLabelRight = (function () {
+        
+        /**
+         * 
+         * @param {CanvasRenderingContext2D} context
+         * @param {Object} fontOptions
+         * @returns {undefined}
+         */
+        var setupCanvasForText = function(context, fontOptions){
+            context.font = fontOptions.style + " " + fontOptions.variant + " " + fontOptions.weight + " " + fontOptions.size + "px '" + fontOptions.family + "'";
+            context.textAlign = "left";
+            context.textBaseline = "bottom";
+        };
+        /**
+         * Estimate the height of the label. Most browsers don't implement
+         * a way to calculate actual text height on the canvas.
+         *
+         * Font options must be set prior to invocation for accurate result
+         * 
+         * @param {CanvasRenderingContext2D} context
+         * @returns {Number}
+         */
+        var calcLabelHeight = function(context){
+            return context.measureText('M').width;
+        };
+        
+        
+        var PADDING = 5;
+        var BOX_LABEL_SPACE = 10;
+        
+        var exports = {};
+        exports.render = function (legendCtx, thisSeries, options, nextEntryOriginX, nextEntryOriginY, fontOptions) {
+            var color = thisSeries.color;
+            var label = thisSeries.label;
+            setupCanvasForText(legendCtx, fontOptions);
+            //calcluate label dims
+            var labelHeight = calcLabelHeight(legendCtx);
+            var boxSize = labelHeight;//square
+            //draw box
+            legendCtx.fillStyle = color;
+            var boxX = nextEntryOriginX + PADDING;
+            var boxY = nextEntryOriginY + PADDING;
+            legendCtx.fillRect(boxX, boxY, boxSize, boxSize);
+            //draw label
+            legendCtx.fillStyle = "#000";
+            var textX = boxX + boxSize + BOX_LABEL_SPACE;
+            // for textY, we need an additional offset of labelHeight because text 
+            // is drawn above and to the right of the coords passed to context.fillText
+            var textY = nextEntryOriginY + PADDING  + labelHeight;
+            legendCtx.fillText(label, textX, textY);
+        };
+        exports.size = function (legendCtx, thisSeries, options, nextEntryOriginX, nextEntryOriginY, fontOptions) {
+            var label = thisSeries.label;
+            setupCanvasForText(legendCtx, fontOptions);
+            var labelHeight = calcLabelHeight(legendCtx);
+            var labelWidth = legendCtx.measureText(label).width;
+            var boxSize = labelHeight;
+
+            return {
+                width: PADDING + boxSize + BOX_LABEL_SPACE + labelWidth + PADDING,
+                height: PADDING + Math.max(labelHeight, boxSize) + PADDING
+            };
+        };
+        return exports;
+    }());
+
+    $.plot.custom_canvas_legend.renderersAndSizers = {
+        boxLeftLabelRight: boxLeftLabelRight
+    };
+}());
---
+++ src/test/resources/specs/jquery.flot.custom-canvas-legend.renderersAndSizersSpec.js
@@ -0,0 +1,79 @@
+/* global $ document describe beforeEach afterEach expect it Math*/
+$(document).ready(function () {
+
+    describe('jquery.flot.custom-canvas-legend.renderersAndSizers.js', function () {
+
+        var customCanvasLegend, plugin, pluginName = 'custom-canvas-legend';
+        //get a reference to the plugin's private methods
+        for (var i = 0; i < $.plot.plugins.length; i++) {
+            plugin = $.plot.plugins[i];
+            if (pluginName === plugin.name) {
+                customCanvasLegend = plugin;
+                break;
+            }
+        }
+        var pluginMethods = customCanvasLegend._private_methods;
+
+        var legendContainer, plotContainer, series, options;
+        var setupDom = function () {
+            plotContainer = $('<div></div>').css({
+                'height': '300px',
+                'width': '500px',
+                'display': 'inline-block'
+            });
+
+            legendContainer = $('<canvas/>').css({
+                'display': 'inline'
+            }).attr({
+                'height': '300',
+                'width': '300'
+            });
+            $('body').append('<h2>' + this.description + '</h2>');
+            $('body').append(plotContainer);
+            $('body').append(legendContainer);
+            var d1 = [];
+            for (var i = 0; i < 14; i += 0.5) {
+                d1.push([i, Math.sin(i)]);
+            }
+
+            var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+            // A null signifies separate line segments
+
+            var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+            series = [{label: 'd1', data: d1}, {label: 'd2', data: d2}, {label: 'd3', data: d3}];
+            options = {
+                legend: {
+                    show: false
+                },
+                canvasLegend: {
+                    show: true,
+                    entrySize: {
+                        height: 50,
+                        width: 50
+                    },
+                    container: legendContainer,
+                    layout: function (seriesIndex, previousEntryOriginX, previousEntryOriginY, previousEntryWidth, previousEntryHeight) {
+                        //simple vertical layout
+                        var nextEntryOriginY = previousEntryOriginY + previousEntryHeight;
+                        return {
+                            nextEntryOriginX: previousEntryOriginX,
+                            nextEntryOriginY: nextEntryOriginY
+                        };
+                    },
+                    margin: 0
+                }
+            };
+        };
+        beforeEach(setupDom);
+        it('should render box left and label right', function () {
+            var boxLeftLabelRight = $.plot.custom_canvas_legend.renderersAndSizers.boxLeftLabelRight;
+            var entryRender = boxLeftLabelRight.render;
+            var entrySize = boxLeftLabelRight.size;
+            
+            options.canvasLegend.entryRender = entryRender;
+            options.canvasLegend.entrySize = entrySize;
+            var plot = $.plot(plotContainer, series, options);
+        });
+    });//root describe block
+});//document ready
